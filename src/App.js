67185d6e7bf66adecece3040df94a6a57d9d69ec
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-10-06 23:40:28 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -8,51 +8,105 @@ const MovieForm = (props) => {
 	const [director, setDirector] = useState("");
 	const [actor, setActor] = useState("");
 	const [plot, setPlot] = useState("");
-	const [imbd, setImbd] = useState(0);
+	const [imdb, setImdb] = useState(0);
 	const [year, setYear] = useState(0);
   
 	return (
 	  <div>
 		<label>Title: </label>
-		<input type="text" onChange={(e)=>{setTitle(e.target.value)}}/>
+		<input type="text" onChange={
+			(e)=>{setTitle(e.target.value)
+		}}/>
 		<br/>
 		<label>Director: </label>
-		<input type="text" onChange={(e)=>{setDirector(e.target.value)}}/>
+		<input type="text" onChange={
+			(e)=>{setDirector(e.target.value)
+		}}/>
 		<br/>
-		<label>Actor: </label>
-		<input type="text" onChange={(e)=>{setActor(e.target.value)}}/>
+		<label>Actors: </label>
+		<input type="text" onChange={
+			(e)=>{setActor(e.target.value)
+		}}/>
 		<br/>
 		<label>Plot: </label>
-		<input type="text" onChange={(e)=>{setPlot(e.target.value)}}/>
+		<input type="text" onChange={
+			(e)=>{setPlot(e.target.value)
+		}}/>
 		<br/>
 		<label>Imdb Rating: </label>
-		<input type="number" step="0.1" onChange={(e)=>{setImbd(e.target.value)}}/>
+		<input type="number" step="0.1" onChange={
+			(e)=>{setImdb(e.target.value)
+		}}/>
 		<br/>
 		<label>Year: </label>
-		<input type="number" onChange={(e)=>{setYear(e.target.value)}}/>
+		<input type="number" onChange={
+			(e)=>{setYear(e.target.value)
+		}}/>
+		<br/>
+		<button onClick={()=>{
+			const newMovie = {
+				title: title,
+				director: director,
+				actor: actor,
+				plot: plot,
+				imdb: imdb,
+				year: year
+			}
+			props.handleAddMovie(newMovie)
+		}}>Add Movie</button>
   
   
 		<p>Current Title: {title}</p>
 		<p>Current Director: {director}</p>
-		<p>Current Actor: {actor}</p>
+		<p>Current Actors: {actor}</p>
 		<p>Current Plot: {plot}</p>
-		<p>Current Imbd Rating: {imbd}</p>
+		<p>Current Imbd Rating: {imdb}</p>
 		<p>Current Year: {year}</p>
 	  </div>
 	)
-  }
+}
   
+const MovieDisplay = (props) => {
+	return (
+		<div>
+			{props.movieList.map((movie, index) => {
+				return <MovieItem movie={movie} key={index}/>
+			})}
+		</div>
+	)
+}
+
+const MovieItem = (props) => {
+	return (
+		<div>
+			<h2>Title: {props.movie.title}</h2>
+			<p>Director: {props.movie.director}</p>
+			<p>Actors: {props.movie.actor}</p>
+			<p>Plot: {props.movie.plot}</p>
+			<p>Imdb Rating: {props.movie.imdb}</p>
+			<p>Year: {props.movie.year}</p>
+		</div>
+	)
+}
   
-  
-  function App() {
+const App = () => {
+
+	const [movieList, setMovieList] = useState([])
+
+	const handleAddMovie = (newMovie) => {
+		const movieListCopy = [ ...movieList, newMovie ]
+		setMovieList(movieListCopy)
+	}
+
 	return (
 	  <div className="App">
 		<h1>Movie Form</h1>
 		<header className="App-header">
-		  <MovieForm/>
+			<MovieForm handleAddMovie={handleAddMovie} />
+			<MovieDisplay movieList={movieList}/>
 		</header>
 	  </div>
 	);
-  }
+}
   
-  export default App;
+export default App;
